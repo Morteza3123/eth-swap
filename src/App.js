580ee5d631f42b4d95210b278bc8341e85fe4abb
Morteza3123 <mkhedry3123@gmail.com>
2021-10-19 12:23:11 +0330

    ll

--- src/App.js
+++ src/App.js
@@ -10,7 +10,16 @@ import './App.css';
 class App extends Component {
 
   loadWeb3(){
-    
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
   }
 
   render(){
